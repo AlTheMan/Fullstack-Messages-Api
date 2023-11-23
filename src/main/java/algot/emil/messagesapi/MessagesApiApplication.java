79b6e7483f8f36f74d36cb5dc77fe1b16306ca67
AlTheMan <algot98@hotmail.com>
2023-11-23 16:52:17 +0100

    bean

--- src/main/java/algot/emil/messagesapi/MessagesApiApplication.java
+++ src/main/java/algot/emil/messagesapi/MessagesApiApplication.java
@@ -1,8 +1,19 @@
 package algot.emil.messagesapi;
 
+import algot.emil.entities.*;
+import algot.emil.enums.EncounterPriority;
+import algot.emil.enums.EncounterStatus;
+import algot.emil.enums.Sex;
+import algot.emil.messagesapi.repositories.MessageRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @EntityScan(basePackages = "algot.emil.entities")
@@ -11,6 +22,16 @@ public class MessagesApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MessagesApiApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initData(MessageRepository messageRepository) {
+
+        return args -> {
+
+            Message message = new Message(3L,1L, 3L,"hej");
+            messageRepository.save(message);
+        };
     }
 
 }
